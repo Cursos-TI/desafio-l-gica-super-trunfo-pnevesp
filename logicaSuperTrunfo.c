397687111907dbd8b469cef4ecf37dac8018691a
pnevesp <164400676+pnevesp@users.noreply.github.com>
2025-05-26 18:40:50 +0000

    Alteração do código incluindo switch e if else

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -3,8 +3,8 @@
 int main(){
     int npt = 50;
     float populacao = 2703.000;
-    float pib = 73000000.000;
-    float area = 313.008;
+    float pib = 73.000000000;
+    float area = 314.930;
     char estado[20] = "Ceara"; 
     char codigo[20] = "A01";
     char cidade[30] = "Fortaleza";
@@ -19,13 +19,13 @@ int main(){
     printf("Área: %.3f\n", area);
     printf("PIB: %.0f\n", pib);
     printf("Número de Pontos Turísticos: %d\n", npt);
-    printf("Densidade Populacional: %.2f\n", densidade, "hab/km²");
-    printf("PIB Per Capita: %.2f\n", pib_per_capita, "reais");
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade);
+    printf("PIB per capita: %.2f hab/km²\n", pib_per_capita);
 
 
     int NPT = 20;
-    float Area = 435.002;
-    float Pib = 98000000.000;
+    float Area = 434.892;
+    float Pib = 98.000000000;
     float Populacao = 1773.000;
     char Estado[20] = "Parana";
     char Codigo[10] = "B02";
@@ -40,26 +40,48 @@ int main(){
     printf("Área: %.3f\n", Area);
     printf("PIB: %.0f\n", Pib);
     printf("Número de Pontos Turísticos: %d\n", NPT);
-    printf("Densidade populacional: %.2f\n", Densidade, "hab/km²");
-    printf("PIB Per Capita: %.2f\n", PIBCAPITA, "reais");
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade);
+    printf("PIB per capita: %.2f hab/km²\n", PIBCAPITA);
+
+    printf("Escolha o atributo que deseja comparar \n");
+    printf("1. Área \n");
+    printf("2. PIB \n");
+
+   int opcao;
+   scanf("%d", &opcao);
+    
+
+   switch (opcao) {
+    case 1:
+        printf("Área de Fortaleza: %.1f km²\n", area);
+        printf("Área de Curitiba: %.1f km²\n", Area);
+        break;
+    case 2:
+        printf("PIB de Fortaleza: %.0f bi\n", pib);
+        printf("PIB de Curitiba: %.0f bi\n", Pib);
+        break;
+    default:
+        printf("Opção Inválida\n");
+}
 
 
-    printf("Comparação de cartas (Atributo: População):\n\n");
-
-
-    printf("Carta 1 %s (%s): %2.f\n", cidade, estado, populacao);
-    printf("Carta 2 %s (%s): %2.f\n\n", Cidade, Estado, Populacao);
-
-      if (populacao > Populacao) {
-        printf("Resultado: Carta 1 (%s) venceu!\n", cidade);
-    } else if (populacao < Populacao) {
-        printf("Resultado: Carta 2 (%s) venceu!\n", Cidade);
-    } else {
-        printf("Resultado: Empate!\n");
-    }
-
-    printf("populacao de Fortaleza = %.3f, Populacao do Paraná = %.3f\n", populacao, Populacao);
-
+    if (opcao == 1) {
+    if (area > Area)
+        printf("Carta 1 (%s) venceu!\n", cidade);
+    else if (area < Area)
+        printf("Carta 2 (%s) venceu!\n", Cidade);
+    else
+        printf("Empate!\n");
+} else if (opcao == 2) {
+    if (pib > Pib)
+        printf("Carta 1 (%s) venceu!\n", cidade);
+    else if (pib < Pib)
+        printf("Carta 2 (%s) venceu!\n", Cidade);
+    else
+        printf("Empate!\n");
+} else {
+    printf("Opções incompatíveis ou inválidas!\n");
+}
   
 
     return 0;
